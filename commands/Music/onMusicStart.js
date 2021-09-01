cf2f42acb906cf10b13c698cd946e7cdd95aaf5f
Elion Wang <74402660+elionwang@users.noreply.github.com>
2021-09-01 10:37:01 +0000

    Commit 09-01-2021

---
+++ commands/Music/onMusicStart.js
@@ -0,0 +1,9 @@
+module.exports = {
+  channel: `$channelID`,
+  type: 'musicStartCommand',
+  code: `$author[$userTag[$clientID];https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif]
+  $description[[$songInfo[title]]($songInfo[url])]
+  $color[23eee2]
+  $title[Now Playing]
+  $replaceText[$replaceText[$pruneMusic;true;];false;]`
+}
--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,5 +1,7 @@
 module.exports = {
   name: 'play',
   aliases: ['p'],
-  code: `$playSong[$message;5m;yes;no;{color:RED}{author:$userTag[$clientID];https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif}{description:Er ging iets fout. Probeer het later opnieuw.}]`
+  code: `$author[$userTag[$clientID];https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif]$title[Added]
+  $color[23eee2]
+  $description[Ik heb $playSong[$message;5m;yes;no;{color:RED}{author:$userTag[$clientID];https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif}{description:Er ging iets fout. Probeer het later opnieuw.}] toegevoegd aan de wachtrij!]`
 }
